079b94e50dd4657fa8972daf4af3a9d8412f17f0
JinsaSameer <jinsajaleel@gmail.com>
2022-08-23 11:53:51 -0700

    updated resume.js

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Header from '../Header';
 import Footer from '../Footer';
 import resume from '../../assets/resume.pdf';
+//import '../../styles/footer.css';
+import '../../styles/style.css';
 export default function Resume() {
     return (
       <div>
@@ -11,14 +13,43 @@ export default function Resume() {
           <div className="container">
             <div className="row">
               <div className="col-md-6">
-                <img className="resume" id="r1" src={resume} alt="pdf of resume"></img>
+                <h3>View my Resume</h3>
+                <a href="https://github.com/JinsaSameer/React_portfolio/blob/main/src/assets/resume.pdf">Resume</a>
+                <div className="col-md-6"><h3>Technical Skills</h3>
+            <div className="row">
+              <div className="col-md-6">
+              <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>Bootstrap</li>
+                <li>JavaScript</li>
+                <li>JQuery</li>
+                <li>React</li>
+                <li>Node.js</li>
+                <li>Express</li>
+              </ul>
+              </div>
+              <div className="col-md-6">
+              <ul>
+                <li>Front-end development</li>
                 
+                <li>MySQL</li>
+                <li>MongoDB</li>
+                <li>Debugging</li>
+                
+                
+              </ul>
               </div>
               </div>
-              </div>
-              </div>
-              <Footer />
-              </div>
-              );
-              }
+            </div>
+          </div>
+        </div>
+      </div>
+      </div>
+      <Footer />
+    </div>
+    );
+}
               
+  
+      
